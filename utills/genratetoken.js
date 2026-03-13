063f81079d9685541c9afd1cfde84108bdb6dfeb
ksuriyamsdgk-max <ksuriyamsdgk@gmail.com>
2026-03-13 11:13:05 +0530

    Delete utills directory

--- utills/genratetoken.js
+++
@@ -1,8 +0,0 @@
-const jwt = require('jsonwebtoken');
-
-const genrateToken = (user) => {
-  return jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-    expiresIn: "7d",
-  });
-};
-module.exports = genrateToken;
